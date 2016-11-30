9e602e0c45db71e648aec9c705cc685fbad1a13b
developer <developer@example.com>
2016-11-30 22:34:37 +0900

    first commit

---
+++ src/com/example/web/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/servlet")
+public class HelloWorld extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello Gradle War Plugin!!");
+    }
+}
